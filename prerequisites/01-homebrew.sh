48ded79d96f6ecb0bb2f4680bd7c7d04b8e5766b
Tim Cheung <tim@cheung.io>
2019-09-03 12:05:17 +0100

    add prerequisites

---
+++ prerequisites/01-homebrew.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Install Homebrew
+#
+# The script explains what it will do and then pauses before it does it.
+#
+# https://brew.sh
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
