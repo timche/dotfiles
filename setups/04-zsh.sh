12db0c5e31108ccf6c0161d66e41d4cec49a783d
Tim Cheung <tim@cheung.io>
2023-06-22 12:22:05 +0100

    update zsh setup

--- setups/04-zsh.sh
+++ setups/04-zsh.sh
@@ -15,4 +15,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 # Plugins
 git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git -~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+# Themes
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
