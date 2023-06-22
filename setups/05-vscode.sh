903467dc9fa7bb7e8095dddc692acaac878582e1
Tim Cheung <tim@cheung.io>
2023-06-22 13:57:01 +0100

    add vscode setup

---
+++ setups/05-vscode.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished
+while true; do
+  sudo -n true
+  sleep 60
+  kill -0 "$$" || exit
+done 2>/dev/null &
+
+# Extensions
+code --install-extension esbenp.prettier-vscode
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension prisma.prisma
