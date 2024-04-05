2e95c5c1f69edaa26b9813e111847169b7782070
Tim Cheung <tim@cheung.io>
2024-04-05 17:27:47 +0100

    use hyper-snazzy theme

--- symlinks/.hyper.js
+++ symlinks/.hyper.js
@@ -138,7 +138,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: ["hyper-snazzy"],
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
